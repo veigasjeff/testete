a9866bdcb0878f3c9e9a459e30cccbb7c1e031bf
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-03-27 03:36:41 +0530

    update

--- components/AdultSkipAds.js
+++ components/AdultSkipAds.js
@@ -79,7 +79,7 @@ function ATFAAds() {
         </div>
       )}
        {remainingTime <= 0 && (
-        <Link href="../../Adult/adult" >
+        <Link href="/" >
       <button onClick={handleClose} type="button" className=" inline-flex items-center rounded-3xl my-5 justify-center p-0.5 mb-2 mr-2 overflow-hidden text-xl font-bold text-gray-900 group bg-gradient-to-br from-red-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 scale-100 hover:scale-110  cursor-pointer relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900  group-hover:bg-opacity-0 ">Exit </button></Link>
     )}
    {remainingTime > 0 && (

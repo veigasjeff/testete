c4b0eb3bac11df36dd1bf3991601709b17abc00f
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-04-24 07:29:43 +0530

    update

--- public/propler/ads.js
+++
@@ -1,1 +0,0 @@
-(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',5741273,document.body||document.documentElement)

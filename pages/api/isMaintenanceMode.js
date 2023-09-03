1ef7876da97b3380dc683ce74ef660a02f261c6b
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-09-03 11:12:50 +0530

    update

--- pages/api/isMaintenanceMode.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-    const isMaintenanceMode = process.env.MAINTENANCE_MODE === "true" ? true : false;
-  
-    res.status(200).json({ isMaintenanceMode });
-  }
-  

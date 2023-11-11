a58c26784036f23f762ae1e561a6bf6ecc69c1ac
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-11-11 16:22:45 +0530

    UPDATE DATA

---
+++ js/tracker.js
@@ -0,0 +1,17 @@
+// googlescript.js
+
+(function () {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-02JT4YS98V';
+  
+    document.head.appendChild(script);
+  
+    script.onload = function () {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', 'G-02JT4YS98V');
+    };
+  })();
+  // K8QKRZ4B44

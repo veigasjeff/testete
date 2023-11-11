705e570cac7794c66e6d2816da1a34d10464fead
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-11-11 16:39:04 +0530

    UPDATE ADS

--- js/ads2.js
+++ js/ads2.js
@@ -3,9 +3,10 @@
         var adScript = document.createElement('script');
         adScript.async = true;
         adScript.setAttribute('data-cfasync', 'false');
-        adScript.src = '//ophoacit.com/1?z=6251332';
+        adScript.src = '//ophoacit.com/1?z=6592151';
         document.body.appendChild(adScript);
       }
 
       // Set a timeout to load the ad code after 5 seconds
       setTimeout(loadAdCode, 8000); // 5000 milliseconds = 5 seconds
+

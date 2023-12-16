7751b4eeb9a5b347de53a20cc05939db5931d907
veigasjeff <110382877+veigasjeff@users.noreply.github.com>
2023-12-17 05:01:37 +0530

    clear update

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://uwatch4free.vercel.app/',
-    changefreq: 'daily',
-    priority: 1.00,
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-  }

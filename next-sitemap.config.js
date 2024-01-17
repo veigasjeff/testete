80a607682b86c4f8ed8625e58c0a2c865da5ed72
veigasjeff <110382877+veigasjeff@users.noreply.github.com>
2024-01-17 12:40:51 +0530

    clear update

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://uwatch4free.vercel.app/',
-    changefreq: 'daily',
-    priority: 0.90,
-    sitemapSize: 7000,
-  }
--- pages/api/movies.js
+++
@@ -1,6 +0,0 @@
-// pages/api/movies.js
-import moviesData from '../../public/movies.json';
-
-export default function handler(req, res) {
-  res.status(200).json(moviesData);
-}

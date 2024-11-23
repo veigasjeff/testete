1f1d11d28e18eff69f61d49e47e2ea8d0554c1c4
jeff <110382877+veigasjeff@users.noreply.github.com>
2024-11-24 04:17:13 +0530

    system updated

--- components/Entertainment.js
+++ components/Entertainment.js
@@ -60,275 +60,355 @@ const VideosPage = () => {
 
   return (
     <div className="videos-page">
-      {/* <div
-        className="videos-page "
-        style={{
+    {/* <div
+      className="videos-page "
+      style={{
+        textShadow: "1px 1px 0px #000",
+        fontSize: "30px",
+        textAlign: "center",
+      }}
+    >
+      <h2   style={{
           textShadow: "1px 1px 0px #000",
           fontSize: "30px",
           textAlign: "center",
+        }} >International News Section.</h2>
+    </div> */}
+    <div className="pagination">
+      <button
+        onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+        disabled={currentPage === 1}
+        style={{
+          textShadow: "1px 1px 0px #000",
+          fontSize: "20px",
+          textAlign: "center",
         }}
       >
-        <h2  style={{
-            textShadow: "1px 1px 0px #000",
-            fontSize: "30px",
-            textAlign: "center",
-          }}>Politics News Section.</h2>
-      </div> */}
-      <div className="pagination">
-        <button
-          onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-          disabled={currentPage === 1}
-          style={{
-            textShadow: "1px 1px 0px #000",
-            fontSize: "20px",
-            textAlign: "center",
-          }} >
-          Previous
-        </button>
-        <span   style={{
+        Previous
+      </button>
+      <span
+        style={{
           textShadow: "1px 1px 0px #000",
           fontSize: "20px",
           textAlign: "center",
-        }}>
-          Page {currentPage} of {totalPages}
-        </span>
-        <button
-          onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
-          disabled={currentPage === totalPages}
-          style={{
-            textShadow: "1px 1px 0px #000",
-            fontSize: "20px",
-            textAlign: "center",
-          }}  >
-          Next
-        </button>
-      </div>
-      <div className="video-grid">
-        {pageVideos.map((video, index) => (
-          <div key={`${video.source}-${index}`} className="video-card">
-            <div className="video-content">
-              <div className="thumbnail">
+        }}
+      >
+        Page {currentPage} of {totalPages}
+      </span>
+      <button
+        onClick={() =>
+          setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+        }
+        disabled={currentPage === totalPages}
+        style={{
+          textShadow: "1px 1px 0px #000",
+          fontSize: "20px",
+          textAlign: "center",
+        }}
+      >
+        Next
+      </button>
+    </div>
+    <div className="video-grid">
+      {pageVideos.map((video, index) => (
+        <div key={`${video.source}-${index}`} className="video-card">
+          <div className="video-content">
+            <div className="thumbnail">
+              <img
+                src={video.thumbnail}
+                alt={video.title}
+                loading="lazy"
+                style={{
+                  width: "200px",
+                  height: "150px",
+                  boxShadow: "0 0 10px 0 #000",
+                  filter:
+                    "contrast(1.1) saturate(1.1) brightness(1.0) hue-rotate(0deg)",
+                }}
+              />
+            </div>
+            <div className="video-info font-bold">
+              <h3
+                style={{
+                  marginBottom: "10px",
+                  textShadow: "1px 1px 0px #000",
+                  fontSize: "30px",
+                }}
+              >
+                {video.title}
+              </h3>
+              <p
+                style={{
+                  marginBottom: "10px",
+                  textShadow: "1px 1px 0px #000",
+                  fontSize: "20px",
+                }}
+              >
+                {video.description}
+              </p>
+              <div
+                style={{
+                  marginBottom: "10px",
+                  textShadow: "1px 1px 0px #000",
+                  fontSize: "20px",
+                }}
+              >
+                Published on {video.date} at {video.time} | Courtesy:{" "}
+                {video.courtesy}
+              </div>
+              {/* Conditional Rendering */}
+              {video.description1 && <p>{video.description1}</p>}
+              {video.image && (
                 <img
-                  src={video.thumbnail}
-                  alt={video.title}
-                  loading="lazy"
+                  src={video.image}
+                  alt={`${video.title} Image`}
                   style={{
-                    width: "200px",
-                    height: "150px",
-                    boxShadow: "0 0 10px 0 #000",
-                    filter:
-                      "contrast(1.1) saturate(1.1) brightness(1.0) hue-rotate(0deg)",
+                    width: "100%",
+                    height: "auto",
+                    marginTop: "10px",
+                    borderRadius: "5px",
                   }}
                 />
-              </div>
-              <div className="video-info font-bold">
-                <h3
-                  style={{
-                    marginBottom: "10px",
-                    textShadow: "1px 1px 0px #000",
-                    fontSize: "30px",
-                  }}
-                >
-                  {video.title}
-                </h3>
-                <p
-                  style={{
-                    marginBottom: "10px",
-                    textShadow: "1px 1px 0px #000",
-                    fontSize: "20px",
-                  }}
-                >
-                  {video.description}
-                </p>
+              )}
+              {video.source && video.source !== "#" && (
+                <div className="player-wrapper">
+                  <div id={`player-${index}`} className="video-player" />
+                </div>
+              )}
+              {/* Embed MP3 Player */}
+              {video.pod && (
                 <div
                   style={{
-                    marginBottom: "10px",
-                    textShadow: "1px 1px 0px #000",
-                    fontSize: "20px",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    marginTop: "20px",
                   }}
                 >
-                  Published on {video.date} at {video.time} | Courtesy:{" "}
-                  {video.courtesy}
+                  {/* Podcast Indicator */}
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      marginBottom: "10px",
+                      fontSize: "18px",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    <span
+                      role="img"
+                      aria-label="Podcast"
+                      style={{
+                        marginRight: "8px",
+                        fontSize: "36px", // Increase font size for larger emoji
+                        lineHeight: "1.5", // Adjust line height for better spacing
+                      }}
+                    >
+                      🎙️
+                    </span>
+                    Podcast
+                  </div>
+
+                  {/* Embed YouTube Player */}
+                  <iframe
+                    width="50%"
+                    height="80"
+                    src={`https://www.youtube.com/embed/${new URL(
+                      video.pod
+                    ).searchParams.get("v")}?list=${new URL(
+                      video.pod
+                    ).searchParams.get("list")}&controls=1`}
+                    frameBorder="0"
+                    allow="encrypted-media"
+                    allowFullScreen
+                    style={{ borderRadius: "5px" }}
+                  />
                 </div>
-                {/* Conditional Rendering */}
-                {video.description1 && <p>{video.description1}</p>}
-                {video.image && (
-                  <img
-                    src={video.image}
-                    alt={`${video.title} Image`}
+              )}
+              {/* Embed MP3 Player */}
+              {video.mp3 && (
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "10px",
+                  }}
+                >
+                     {/* Podcast Indicator */}
+                     <div
                     style={{
-                      width: "100%",
-                      height: "auto",
-                      marginTop: "10px",
-                      borderRadius: "5px",
+                      display: "flex",
+                      alignItems: "center",
+                      marginBottom: "10px",
+                      marginRight: "8px",
+                      fontSize: "18px",
+                      fontWeight: "bold",
                     }}
-                  />
-                )}
-                   {video.source && video.source !== "#" && (
-              <div className="player-wrapper">
-                <div id={`player-${index}`} className="video-player" />
-              </div>
-            )}
-                {video.description2 && <p>{video.description2}</p>}
-                {video.image1 && (
-                  <img
-                    src={video.image1}
-                    alt={`${video.title} Image 1`}
-                    style={{
-                      width: "100%",
-                      height: "auto",
-                      marginTop: "10px",
-                      borderRadius: "5px",
-                    }}
-                  />
-                )}
-              </div>
+                  >
+                    <span
+                      role="img"
+                      aria-label="Podcast"
+                      style={{
+                        marginRight: "8px",
+                        fontSize: "36px", // Increase font size for larger emoji
+                        lineHeight: "1.5", // Adjust line height for better spacing
+                      }}
+                    >
+                      🎙️
+                    </span>
+                    Podcast
+                  </div>
+                  <audio controls style={{ width: "50%" }}>
+                    <source src={video.mp3} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                  </audio>
+                </div>
+              )}
+              {video.description2 && <p>{video.description2}</p>}
+              {video.image1 && (
+                <img
+                  src={video.image1}
+                  alt={`${video.title} Image 1`}
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                    marginTop: "10px",
+                    borderRadius: "5px",
+                  }}
+                />
+              )}
             </div>
-            {/* {video.source && video.source !== "#" && (
-              <div className="player-wrapper">
-                <div id={`player-${index}`} className="video-player" />
-              </div>
-            )} */}
           </div>
-        ))}
-      </div>
-      {/* Pagination */}
-      <div className="pagination">
-        <button
-          onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-          disabled={currentPage === 1}
-          style={{
-            textShadow: "1px 1px 0px #000",
-            fontSize: "20px",
-            textAlign: "center",
-          }} >
-          Previous
-        </button>
-        <span   style={{
+        </div>
+      ))}
+    </div>
+    {/* Pagination */}
+    <div className="pagination">
+      <button
+        onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+        disabled={currentPage === 1}
+        style={{
           textShadow: "1px 1px 0px #000",
           fontSize: "20px",
           textAlign: "center",
-        }}>
-          Page {currentPage} of {totalPages}
-        </span>
-        <button
-          onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
-          disabled={currentPage === totalPages}
-          style={{
-            textShadow: "1px 1px 0px #000",
-            fontSize: "20px",
-            textAlign: "center",
-          }}  >
-          Next
-        </button>
-      </div>
-
-        <style jsx>{`
-        .videos-page {
-          padding: 20px;
+        }}
+      >
+        Previous
+      </button>
+      <span
+        style={{
+          textShadow: "1px 1px 0px #000",
+          fontSize: "20px",
+          textAlign: "center",
+        }}
+      >
+        Page {currentPage} of {totalPages}
+      </span>
+      <button
+        onClick={() =>
+          setCurrentPage((prev) => Math.min(prev + 1, totalPages))
         }
+        disabled={currentPage === totalPages}
+        style={{
+          textShadow: "1px 1px 0px #000",
+          fontSize: "20px",
+          textAlign: "center",
+        }}
+      >
+        Next
+      </button>
+    </div>
 
-        .video-grid {
-          display: flex;
-          flex-direction: column;
-          gap: 20px;
-        }
+    <style jsx>{`
+      .videos-page {
+        padding: 20px;
+      }
 
-        .video-card {
-          border: 1px solid #ddd;
-          border-radius: 5px;
-          padding: 10px;
-          background-color: #fff;
-        }
+      .video-grid {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+      }
 
+      .video-card {
+        border: 1px solid #ddd;
+        border-radius: 5px;
+        padding: 10px;
+        background-color: #fff;
+      }
+
+      .video-content {
+        display: flex;
+        flex-direction: row;
+        align-items: flex-start;
+        gap: 15px;
+      }
+
+      .thumbnail img {
+        width: 150px;
+        height: auto;
+        border-radius: 5px;
+      }
+
+      .video-info {
+        flex: 1;
+      }
+
+      .player-wrapper {
+        margin-top: 10px;
+        position: relative;
+        padding-top: 56.25%; /* 16:9 Aspect Ratio */
+      }
+
+      .video-player {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+
+      .extra-content {
+        margin-top: 15px;
+      }
+
+      .pagination {
+        margin-top: 20px;
+        text-align: center;
+      }
+
+      .pagination button {
+        margin: 0 5px;
+        padding: 10px 15px;
+        border: none;
+        background-color: #007bff;
+        color: #fff;
+        cursor: pointer;
+        border-radius: 5px;
+      }
+
+      .pagination button:hover {
+        background-color: #0056b3;
+      }
+
+      .pagination span {
+        margin: 0 10px;
+      }
+
+      @media (max-width: 768px) {
         .video-content {
-          display: flex;
-          flex-direction: row;
-          align-items: flex-start;
-          gap: 15px;
+          flex-direction: column;
+          align-items: stretch;
         }
 
         .thumbnail img {
-          width: 150px;
-          height: auto;
-          border-radius: 5px;
-        }
-
-        .video-info {
-          flex: 1;
-        }
-
-        .player-wrapper {
-          margin-top: 10px;
-          position: relative;
-          padding-top: 56.25%; /* 16:9 Aspect Ratio */
-        }
-
-        .video-player {
-          position: absolute;
-          top: 0;
-          left: 0;
           width: 100%;
-          height: 100%;
+          height: auto;
         }
-
-        .extra-content {
-          margin-top: 15px;
-        }
-
-        .pagination {
-          margin-top: 20px;
-          text-align: center;
-        }
-
-        .pagination button {
-          margin: 0 5px;
-          padding: 10px 15px;
-          border: none;
-          background-color: #007bff;
-          color: #fff;
-          cursor: pointer;
-          border-radius: 5px;
-        }
-
-        .pagination button:hover {
-          background-color: #0056b3;
-        }
-
-        .pagination span {
-          margin: 0 10px;
-        }
-
-        @media (max-width: 768px) {
-          .video-content {
-            flex-direction: column;
-            align-items: stretch;
-          }
-
-          .thumbnail img {
-            width: 100%;
-            height: auto;
-          }
-        }
-      `}</style>
-    </div>
-  );
+      }
+    `}</style>
+  </div>
+);
 };
 
 export default VideosPage;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
